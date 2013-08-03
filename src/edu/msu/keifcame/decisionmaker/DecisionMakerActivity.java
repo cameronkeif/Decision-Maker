75cde1ad43be6e913b6146a8152ac8ff58213248
Cameron Keif <cameron.keif@gmail.com>
2013-08-03 02:47:53 -0400

    Hook up onClick calls

--- src/edu/msu/keifcame/decisionmaker/DecisionMakerActivity.java
+++ src/edu/msu/keifcame/decisionmaker/DecisionMakerActivity.java
@@ -1,8 +1,9 @@
 package edu.msu.keifcame.decisionmaker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class DecisionMakerActivity extends Activity {
 
@@ -17,6 +18,20 @@ public class DecisionMakerActivity extends Activity {
       // Inflate the menu; this adds items to the action bar if it is present.
       getMenuInflater().inflate( R.menu.activity_decision_maker, menu );
       return true;
+   }
+   
+   // Handles an add choice click
+   public void onClickAddChoice( View v ) {
+   }
+   
+   // Handles an add choice click
+   public void onClickMakeDecision( View v ) {
+      
+   }
+   
+   // Handles an add choice click
+   public void onClickReset( View v ) {
+      
    }
 
 }
