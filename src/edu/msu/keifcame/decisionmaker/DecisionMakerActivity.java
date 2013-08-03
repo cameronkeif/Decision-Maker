888b2afd68807bd0a2939ef45bb28e9fd69ac786
Cameron Keif <cameron.keif@gmail.com>
2013-08-03 02:49:48 -0400

    lint--

--- src/edu/msu/keifcame/decisionmaker/DecisionMakerActivity.java
+++ src/edu/msu/keifcame/decisionmaker/DecisionMakerActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.EditText;
 import android.widget.LinearLayout;
